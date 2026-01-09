6072c158a3e8a64f6833769b859d9425c0476ee7
jdlandsh <jurgen.delandsheer@ugent.be>
2026-01-09 17:35:31 +0100

    javascript runtime / pack mngrs

--- test-run-ts-write.bat
+++ test-run-ts-write.bat
@@ -3,6 +3,10 @@ cls
 
 cd bun
 cd vite-project
+del output1.txt 2>nul
+del output2.txt 2>nul
+del output3.txt 2>nul
+del output4.txt 2>nul
 echo on
 bun run test-ts-write.ts
 @echo off
@@ -18,6 +22,10 @@ echo ==================================
 
 cd deno
 cd vite-project
+del output1.txt 2>nul
+del output2.txt 2>nul
+del output3.txt 2>nul
+del output4.txt 2>nul
 echo on
 deno run --allow-write test-ts-write.ts
 @echo off
@@ -33,6 +41,10 @@ echo ==================================
 
 cd node
 cd vite-project
+del output1.txt 2>nul
+del output2.txt 2>nul
+del output3.txt 2>nul
+del output4.txt 2>nul
 echo on
 node --experimental-strip-types test-ts-write.ts
 @echo off
