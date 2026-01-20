581481fde221815380180a27462441cb96b64848
jdlandsh <jurgen.delandsheer@ugent.be>
2026-01-20 11:08:15 +0100

    javascript runtime / pack mngrs

--- install-deno.bat
+++ install-deno.bat
@@ -1,9 +1,11 @@
-::run in Win Powershell
-::irm https://deno.land/install.ps1 | iex
-
-::run in Win command prompt: force Powershell
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-	"irm https://deno.land/install.ps1 -OutFile deno-install-tmp.ps1; .\deno-install-tmp.ps1"
-@del .\deno-install-tmp.ps1
-
+@echo run in Win Powershell
+@echo "irm https://deno.land/install.ps1 | iex"
+@echo or continue
+@pause
+powershell ^
+  -NoProfile ^
+  -NonInteractive ^
+  -ExecutionPolicy Bypass ^
+  -Command ^
+  "$ProgressPreference='SilentlyContinue'; $ErrorActionPreference='Stop'; irm https://deno.land/install.ps1 -UseBasicParsing -OutFile deno-install-tmp.ps1; powershell -NoProfile -ExecutionPolicy Bypass -File .\deno-install-tmp.ps1; Remove-Item deno-install-tmp.ps1 -Force"
 @pause
