0aa275823beb7c69d54753ab7e8e9b54bb6f7aef
jdlandsh <jurgen.delandsheer@ugent.be>
2026-02-06 16:26:06 +0100

    vite config

--- deno/vite-project/vite.config.ts
+++ deno/vite-project/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	base: './', // fixed index.html having sources start with "/" instead of "./" which breaks everything except root deployment
+	css: {
+		modules: {
+			localsConvention: 'camelCaseOnly',
+			// or: 'camelCase' (keeps kebab-case too)
+		},
+	},
 })
